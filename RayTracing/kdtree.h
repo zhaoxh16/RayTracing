83a2ec1a5e5be27085995b782e1baa1d64bbb8e5
zxh <zhaoxinhao2012@126.com>
2018-05-27 09:36:58 +0800

    kdtree模板类

--- RayTracing/kdtree.h
+++ RayTracing/kdtree.h
@@ -5,23 +5,40 @@
 
 using namespace std;
 
+template<typename T>
 class Node {
 public:
-	Photon * photon;
-	Node* p;
-	Node* ch[2];
+	T* obj;
+	Node<T>* p;
+	Node<T>* ch[2];
 	bool isLC;
 
-	Node();
-	Node(Photon* photon);
-	void addChild(Node* child, int LR);//LR为0或1，表示child的位置
+	Node() :obj(NULL), p(NULL), isLC(false) {
+		ch[0] = NULL;
+		ch[1] = NULL;
+	}
+
+	Node(T* obj) : obj(obj), p(NULL), isLC(false) {
+		ch[0] = NULL;
+		ch[1] = NULL;
+	}
+
+	void addChild(Node<T>* child, int LR) {
+		//LR为0或1，表示child的位置
+		if (child == NULL) return;
+		child->p = this;
+		ch[LR] = child;
+		if (LR == 0) child->isLC = true;
+		else child->isLC = false;
+	}
 };
 
+template<typename T>
 class Compare {
 public:
 	Compare(int index):index(index){}
-	bool operator() (const Node* a, const Node* b) {
-		if (a->photon->pos[index] < b->photon->pos[index]) return true;
+	bool operator() (const Node<T>* a, const Node<T>* b) {
+		if (a->obj->pos[index] < b->obj->pos[index]) return true;
 		return false;
 	}
 
@@ -29,16 +46,45 @@ private:
 	int index;
 };
 
+template<typename T>
 class KDTree {
 public:
-	KDTree();
-	~KDTree();
-	void addNode(Photon* photon);
-	void build();
+	KDTree():root(NULL){}
+	~KDTree() {
+		for (int i = 0; i < nodes.size(); ++i)
+			if (nodes[i]) delete nodes[i];
+	}
+	void addNode(T* obj) {
+		Node<T>* node = new Node<T>(obj);
+		nodes.push_back(node);
+	}
+	void build() {
+		int count = nodes.size();
+		if (count == 0) return;
+		else if (count == 1) {
+			root = nodes[0];
+			return;
+		}
+		else root = split(nodes.begin(), nodes.end(), 0);
+	}
 
 private:
-	Node* root;
-	vector<Node*> nodes;
-	Node* split(vector<Node*>::iterator begin, vector<Node*>::iterator end, int index);//对[begin,end)按照第index个坐标进行划分，并返回根节点
+	Node<T>* root;
+	vector<Node<T>*> nodes;
+	Node<T>* split(typename vector<Node<T>*>::iterator begin, typename vector<Node<T>*>::iterator end, int index) {
+		//对[begin,end)按照第index个坐标进行划分，并返回根节点
+		if (end - begin == 0) return NULL;
+		else if (end - begin == 1) {
+			cout << (*begin)->obj->pos[0] << (*begin)->obj->pos[1] << (*begin)->obj->pos[2] << endl;
+			return *begin;
+		}
+		Compare cmp(index);
+		nth_element(begin, begin + (end - begin) / 2, end, cmp);
+		Node* subRoot = *(begin + (end - begin) / 2);
+		cout << subRoot->obj->pos[0] << subRoot->obj->pos[1] << subRoot->obj->pos[2] << endl;
+		subRoot->addChild(split(begin, begin + (end - begin) / 2, (index + 1) % 3), 0);
+		subRoot->addChild(split(begin + (end - begin) / 2 + 1, end, (index + 1) % 3), 1);
+		return subRoot;
+	}
 
 };
